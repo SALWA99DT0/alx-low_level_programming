c384bd0e7621d637253cf35410d2b706ee062019
salwua <salouamhs@gmail.com>
2023-04-26 13:57:37 -0700

    ./2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print alphabet x10 times
+*/
+void print_alphabet_x10(void)
+{
+	char i, line;
+{
+for (line = 0; line <= 9; line++)
+
+for (i = 'a' ; i <= 'z' ; i++)
+_putchar(i);
+_putchar('\n');
+}
+}
