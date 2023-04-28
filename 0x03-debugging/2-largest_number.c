76e920873112c26e442b621f6b3241d95170829d
salwua <salouamhs@gmail.com>
2023-04-28 08:20:31 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,27 +2,21 @@
 
 /**
 * largest_number - returns the largest of 3 numbers
-*
-* function takes three integer arguments and returns the largest of the three.
-* function takes largest(int a, int b, int c) and returns the largest of 3
 * @a: first integer
 * @b: second integer
 * @c: third integer
-*
 * Return: largest number
 */
-int largest(int a, int b, int c)
+
+int largest_number(int a, int b, int c)
 {
+
 int largest;
-if (a > b && a > c)
+if (a >= b && a >= c)
 {
 largest = a;
 }
-else if (a > b  && c > a)
-{
-largest = c;
-}
-else if (b > c)
+else if (b >= a && b >= c)
 {
 largest = b;
 }
--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -2,38 +2,15 @@
 #include "main.h"
 
 /**
-* largest_number - returns the largest of three numbers
-* @a: first integer
-* @b: second integer
-* @c: third integer
-* Return: largest number
+* main - prints the largest of 3 integers
+* Return: 0
 */
-int largest_number(int a, int b, int c)
-{
-int largest = c;
-if (b > largest)
-{
-largest = b;
-}
-if (a  > largest)
-{
-largest = a;
-}
-return (largest);
-}
-/**
- * main - Entry point for the program
- *
- * function initializes three integers and calls the largest_number function
- * to find the largest of the three. The result is printed to the console.
- *
- * Return: Always 0 (success)
- */
+
 int main(void)
 {
 int a, b, c;
 int largest;
-a = -972;
+a = 972;
 b = -98;
 c = 0;
 largest = largest_number(a, b, c);
