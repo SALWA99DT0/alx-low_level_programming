0917114bc095e38c7241aed7d89b8aed920cd309
salwua <salouamhs@gmail.com>
2023-05-05 15:45:46 -0700

    ./101-print_numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,23 +3,22 @@
 /**
 *print_number - function that prints an integer.
 *@n: the integer to print
-*Return: void
 */
 void print_number(int n)
 {
-unsigned int num;
-if (n < 0)
-{
-_putchar('_');
-num = -n;
-}
-else
-{
-num = n;
-}
-if (num / 10 != 0)
-{
-print_number(num / 10);
-}
-_putchar (num % 10 + '0');
+	unsigned int num;
+	if (n < 0)
+	{
+		_putchar('_');
+		num = -n;
+	}
+	else
+	{
+	num = n;
+	}
+	if (num / 10 != 0)
+	{
+	print_number(num / 10);
+	}
+	_putchar (num % 10 + '0');
 }
