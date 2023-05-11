fee77f5e7bde0e69f879d13478f209a71ce777dd
salwua <salouamhs@gmail.com>
2023-05-11 12:06:00 -0700

    ./4-pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+/**
+*_pow_recursion - prints a function that returns,
+*the value of x raised to the power of y.
+*@y: the exponent.
+*@x: the base.
+*Return: If y is lower than 0, the function should return -1
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	return (x * pow(x, y - 1));
+}
