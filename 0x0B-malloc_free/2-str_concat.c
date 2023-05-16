727f1073eef8d0c3a4b2dedba515e1139aa65ac7
salwua <salouamhs@gmail.com>
2023-05-16 13:14:56 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,27 +9,24 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-	size_t len1, len2;
+	size_t len1 = 0, len2 = 0;
 	char *result;
 
 	if (s1 == NULL)
-{
-	s1 = " ";
-}
+		len1 = strlen(s1);
 	if (s2 == NULL)
-{
-	s2 = " ";
-}
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+		len2 = strlen(s2);
 
-	result = malloc((len1 + len2 + 1) * sizeof(char));
+	result = (char *) malloc((len1 + len2 + 1) * sizeof(char));
 
 	if (result == NULL)
-{
-	return (NULL);
-}
-	strcpy(result, s1);
-	strcat(result, s2);
+		return (NULL);
+	if (s1 != NULL)
+		strcpy(result, s1);
+	else
+	result[0] = '\0';
+	if (s2 != NULL)
+		strcat(result, s2);
+
 	return (result);
 }
