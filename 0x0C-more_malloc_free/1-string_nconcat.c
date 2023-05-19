c684c21d217b9258b29c83226cb4ff19b578db43
salwua <salouamhs@gmail.com>
2023-05-19 15:52:40 -0700

    ./1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,34 +10,28 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	size_t s1_len = strlen(s1);
-	size_t s2_len = strlen(s2);
-	size_t concat_len = s1_len + n;
-	char *concat_str;
-	size_t j;
-	size_t i;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	char *s;
 
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	if (n >= s2_len)
-	n = s2_len;
-
-	concat_str = malloc((concat_len + 1) * sizeof(char));
-
-	for (i = 0; i < s1_len; i++)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!s)
+		return (NULL);
+	while (i < len1)
 	{
-		concat_str[i] = s1[i];
+		s[i] = s1[i];
+		i++;
 	}
-
-	for (j = 0; j < n; j++)
-	{
-		concat_str[i++] = s2[j];
-	}
-
-	concat_str[i] = '\0';
-	return (concat_str);
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+	s[i] = '\0';
+	return (s);
 }
