e7c385182df6d489b81e385031f9d7394c177827
salwua <salouamhs@gmail.com>
2023-05-22 15:52:41 -0700

    dog basics

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog - dog information
+*@name: first member
+*@age: second member
+*@owner: third member
+*Description: oky my description
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
