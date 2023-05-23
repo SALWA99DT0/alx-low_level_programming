4503140123e5c699c68745d311f0e571295f7938
salwua <salouamhs@gmail.com>
2023-05-23 14:52:59 -0700

    dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,12 +13,12 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
-void print_dog(dog_t *d);
-#endif
+};
+
 /**
-*dog_t - typdef for struct dog
+* dog_t - typedef for struct dog
 */
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+#endif
