d3b250a588bfe5a8b53e573038cec37f6701a442
salwua <salouamhs@gmail.com>
2023-06-12 16:16:52 -0700

    creat

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,28 +10,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, bytes_written;
-	mode_t mode = S_IRUSR | S_IWUSR;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
-	return (-1);
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
-	if (fd == -1)
-	return (-1);
+		return (-1);
 
 	if (text_content != NULL)
 	{
-		while (*text_content)
-		text_content++;
-
-	bytes_written = write(fd, text_content, text_content - text_content);
-	if (bytes_written == -1 || bytes_written != text_content - text_content)
-		{
-		close(fd);
-		return (-1);
-		}
+		for (len = 0; text_content[len];)
+			len++;
 	}
-		close(fd);
-		return (1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
